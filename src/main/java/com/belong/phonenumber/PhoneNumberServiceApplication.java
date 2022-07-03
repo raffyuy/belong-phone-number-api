b1cdfd86d9efce7252a38f74231775dc2819eaca
Rafael <raffyuy@gmail.com>
2022-07-03 18:07:35 +1000

    Initial files

---
+++ src/main/java/com/belong/phonenumber/PhoneNumberServiceApplication.java
@@ -0,0 +1,13 @@
+package com.belong.phonenumberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneNumberServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneNumberServiceApplication.class, args);
+	}
+
+}
