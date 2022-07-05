f1eb266555f28ec4144fa98de8dce15ff108bced
Rafael <raffyuy@gmail.com>
2022-07-06 03:10:42 +1000

    Add swagger docs

--- src/main/java/com/belong/phonenumber/controller/CustomerController.java
+++ src/main/java/com/belong/phonenumber/controller/CustomerController.java
@@ -1,7 +1,14 @@
 package com.belong.phonenumber.controller;
 
 import com.belong.phonenumber.model.Customer;
+import com.belong.phonenumber.model.PhoneNumber;
 import com.belong.phonenumber.repository.CustomerRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -19,10 +27,17 @@ public class CustomerController {
     private CustomerRepository customerRepository;
 
     /**
-     * Retrieve all phone numbers belonging to a customer
+     * Get all phone numbers belonging to a customer
      * @param id - The customer id
      * @return A list of phone numbers belonging to the customer, or a 404 not found if the customer record is not found
      */
+    @Operation(summary = "Get all phone numbers belonging to a customer given a customer Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the customer",
+                    content = { @Content(mediaType = "application/json",
+                        array = @ArraySchema(schema = @Schema(implementation = PhoneNumber.class))) }),
+            @ApiResponse(responseCode = "404", description = "Customer not found",
+                    content = @Content) })
     @GetMapping("/customers/{id}/phonenumbers")
     ResponseEntity<?> getPhoneNumbersByCustomerId(@PathVariable Long id) {
         Optional<Customer> customerById = customerRepository.findById(id);
--- src/main/java/com/belong/phonenumber/controller/PhoneNumberController.java
+++ src/main/java/com/belong/phonenumber/controller/PhoneNumberController.java
@@ -2,6 +2,12 @@ package com.belong.phonenumber.controller;
 
 import com.belong.phonenumber.model.PhoneNumber;
 import com.belong.phonenumber.repository.PhoneNumberRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,11 +32,30 @@ public class PhoneNumberController {
      * Lists all phone numbers in the database sorted by phone number
      * @return
      */
+    @Operation(summary = "Get all phone numbers")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Phone numbers retrieved",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = PhoneNumber.class))) }),
+    })
     @GetMapping("/phonenumbers")
     List<PhoneNumber> phoneNumbers() {
         return phoneNumberRepository.findAll(Sort.by(Sort.Direction.ASC, "phoneNumber"));
     }
 
+    /**
+     * Given a phone number, sets the PhoneNumber's isActive status to true.
+     *
+     * @param id - String - an existing phone number
+     * @return PhoneNumber record with updated isActive status
+     */
+    @Operation(summary = "Activate a phone number if it exists")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the phone number and activated it",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PhoneNumber.class)) }),
+            @ApiResponse(responseCode = "404", description = "Phone number not found",
+                    content = @Content) })
     @PostMapping("/phonenumbers/{id}/activate")
     ResponseEntity<?> activatePhoneNumber(@PathVariable String id) {
         log.info("Updating phone number: {}", id);
